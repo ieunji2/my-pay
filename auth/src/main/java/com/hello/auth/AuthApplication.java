8e3fa6390c24736449a0fea0344c31862398b663
LEEEUNJI <eunjilee3008@gmail.com>
2023-09-03 16:40:48 +0900

    account, auth, api-gateway 프로젝트 추가

--- auth/src/main/java/com/hello/auth/AuthApplication.java
+++ auth/src/main/java/com/hello/auth/AuthApplication.java
@@ -1,13 +1,13 @@
-package com.hello.mypay;
+package com.hello.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyPayApplication {
+public class AuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyPayApplication.class, args);
+		SpringApplication.run(AuthApplication.class, args);
 	}
 
 }
