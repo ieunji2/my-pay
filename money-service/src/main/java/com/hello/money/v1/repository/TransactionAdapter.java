f5f90d588f3b434a0a7acbf71fbfc7de16fc29ec
LEEEUNJI <eunjilee3008@gmail.com>
2023-10-18 14:13:28 +0900

    [#6] refactor: lombok RequiredArgsConstructor 어노테이션 적용

--- money-service/src/main/java/com/hello/money/v1/repository/TransactionAdapter.java
+++ money-service/src/main/java/com/hello/money/v1/repository/TransactionAdapter.java
@@ -2,16 +2,14 @@ package com.hello.money.v1.repository;
 
 import com.hello.money.domain.Transaction;
 import com.hello.money.v1.service.TransactionPort;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 class TransactionAdapter implements TransactionPort {
 
   private final TransactionRepository transactionRepository;
-
-  TransactionAdapter(final TransactionRepository transactionRepository) {
-    this.transactionRepository = transactionRepository;
-  }
 
   @Override
   public Transaction saveTransaction(final Transaction transaction) {
