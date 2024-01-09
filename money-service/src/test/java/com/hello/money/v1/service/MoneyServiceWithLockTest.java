cd94f16f2c789a97a315f0ebdd4154f400487154
LEEEUNJI <eunjilee3008@gmail.com>
2024-01-09 23:42:10 +0900

    [#13] refactor: 동시성 테스트 요청 횟수 변경 - 기존에 테스트 별 1000번씩 요청으로 프로젝트 빌드 시 시간이 너무 오래 걸림

--- money-service/src/test/java/com/hello/money/v1/service/MoneyServiceWithLockTest.java
+++ money-service/src/test/java/com/hello/money/v1/service/MoneyServiceWithLockTest.java
@@ -6,7 +6,6 @@ import com.hello.money.v1.dto.SendMoneyServiceDto;
 import com.hello.money.v1.repository.TransactionRepository;
 import com.hello.money.v1.repository.WalletRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -45,31 +44,47 @@ class MoneyServiceWithLockTest {
 
   private static Stream<Arguments> chargeMoneyServiceDtoParam() {
     return Stream.of(
-            arguments(new ChargeMoneyServiceDto(1L, "이름", BigInteger.valueOf(3000), "적요")));
+//            arguments(
+//                    new ChargeMoneyServiceDto(1L, "이름", BigInteger.valueOf(3000), "적요"), 1000, 3000000),
+            arguments(
+                    new ChargeMoneyServiceDto(1L, "이름", BigInteger.valueOf(3000), "적요"), 100, 300000));
   }
 
   private static Stream<Arguments> sendMoneyServiceDtoParam() {
     return Stream.of(
-            arguments(new SendMoneyServiceDto(1L, "이름", 2L, BigInteger.ONE, "적요")));
+//            arguments(
+//                    new SendMoneyServiceDto(1L, "이름", 2L, BigInteger.ONE, "적요"), 1000, 0, 1000),
+            arguments(
+                    new SendMoneyServiceDto(1L, "이름", 2L, BigInteger.ONE, "적요"), 100, 900, 100));
   }
 
   private static Stream<Arguments> chargeMoneyAndSendMoneyServiceDtoParam() {
     return Stream.of(
+//            arguments(
+//                    new SendMoneyServiceDto(1L, "이름", 2L, BigInteger.ONE, "적요"),
+//                    new ChargeMoneyServiceDto(2L, "이름2", BigInteger.valueOf(3000), "적요"),
+//                    new SendMoneyServiceDto(3L, "이름", 1L, BigInteger.ONE, "적요"),
+//                    300,
+//                    3000,
+//                    303100,
+//                    2900),
             arguments(
                     new SendMoneyServiceDto(1L, "이름", 2L, BigInteger.ONE, "적요"),
                     new ChargeMoneyServiceDto(2L, "이름2", BigInteger.valueOf(3000), "적요"),
-                    new SendMoneyServiceDto(3L, "이름", 1L, BigInteger.ONE, "적요")));
+                    new SendMoneyServiceDto(3L, "이름", 1L, BigInteger.ONE, "적요"),
+                    30,
+                    3000,
+                    33010,
+                    2990));
   }
 
-  @ParameterizedTest
+  @ParameterizedTest(name = "@DistributedLock 적용한 머니충전 {1}번 동시 요청 시 잔액이 {2}와 같다.")
   @MethodSource("chargeMoneyServiceDtoParam")
-  @DisplayName("@DistributedLock 적용한 머니충전 동시 요청 시 잔액이 기대와 같다.")
-  public void 분산_락_머니충전_동시에_1000번_요청_후_잔액_확인(final ChargeMoneyServiceDto dto) throws InterruptedException {
+  public void 분산_락_머니충전_동시_요청_후_잔액_확인(final ChargeMoneyServiceDto dto, int threadCount, int balance) throws InterruptedException {
     //given
     walletPort.saveWallet(new Wallet(dto.accountId()));
 
     //when
-    final int threadCount = 1000;
     final ExecutorService executorService = Executors.newFixedThreadPool(32);
     final CountDownLatch latch = new CountDownLatch(threadCount);
 
@@ -87,22 +102,20 @@ class MoneyServiceWithLockTest {
 
     //then
     final Wallet wallet = walletPort.findWalletByAccountId(dto.accountId());
-    assertThat(wallet.getBalance()).isEqualTo(3000000);
+    assertThat(wallet.getBalance()).isEqualTo(balance);
   }
 
-  @ParameterizedTest
+  @ParameterizedTest(name = "@DistributedMultiLock 적용한 머니송금 {1}번 동시 요청 시 송금인 잔액이 {2}와 같다.")
   @MethodSource("sendMoneyServiceDtoParam")
-  @DisplayName("@DistributedMultiLock 적용한 머니송금 동시 요청 시 잔액이 기대와 같다.")
-  public void 분산_락_머니송금_동시에_1000번_요청_후_잔액_확인(final SendMoneyServiceDto dto) throws InterruptedException {
+  public void 분산_락_머니송금_동시_요청_후_잔액_확인(final SendMoneyServiceDto dto, int threadCount, int senderBalance, int receiverBalance) throws InterruptedException {
     //given
     final Wallet senderWallet = walletPort.saveWallet(new Wallet(dto.accountId()));
-    senderWallet.addMoney(BigInteger.valueOf(3000));
+    senderWallet.addMoney(BigInteger.valueOf(1000));
     walletPort.saveWallet(senderWallet);
 
     final Wallet receiverWallet = walletPort.saveWallet(new Wallet(2L));
 
     //when
-    final int threadCount = 1000;
     final ExecutorService executorService = Executors.newFixedThreadPool(32);
     final CountDownLatch latch = new CountDownLatch(threadCount);
 
@@ -131,19 +144,21 @@ class MoneyServiceWithLockTest {
     final Wallet savedReceiverWallet = walletPort.findWalletById(receiverWallet.getId());
     System.out.println("savedReceiverWallet.getBalance() = " + savedReceiverWallet.getBalance());
 
-    assertThat(savedSenderWallet.getBalance()).isEqualTo(BigInteger.valueOf(2000));
-    assertThat(savedReceiverWallet.getBalance()).isEqualTo(BigInteger.valueOf(1000));
+    assertThat(savedSenderWallet.getBalance()).isEqualTo(BigInteger.valueOf(senderBalance));
+    assertThat(savedReceiverWallet.getBalance()).isEqualTo(BigInteger.valueOf(receiverBalance));
   }
 
-  @ParameterizedTest
+  @ParameterizedTest(name = "@DistributedMultiLock 적용한 머니충전 및 머니송금 {3}번 동시 요청 시 잔액이 {4}, {5}, {6}와 같다.")
   @MethodSource("chargeMoneyAndSendMoneyServiceDtoParam")
-  @DisplayName("@DistributedMultiLock 적용한 머니충전 및 머니송금 동시 요청 시 잔액이 기대와 같다.")
-  void 분산_락_머니충전_머니송금_동시에_300번_요청_후_잔액_확인(
+  void 분산_락_머니충전_머니송금_동시_요청_후_잔액_확인(
           final SendMoneyServiceDto dto1,
           final ChargeMoneyServiceDto dto2,
-          final SendMoneyServiceDto dto3) throws InterruptedException {
+          final SendMoneyServiceDto dto3,
+          int threadCount,
+          int balance1,
+          int balance2,
+          int balance3) throws InterruptedException {
 
-    //given: 1(3000), 2(3000), 3(3000)
     final Wallet wallet1 = walletPort.saveWallet(new Wallet(dto1.accountId()));
     wallet1.addMoney(BigInteger.valueOf(3000));
     walletPort.saveWallet(wallet1);
@@ -156,16 +171,15 @@ class MoneyServiceWithLockTest {
     wallet3.addMoney(BigInteger.valueOf(3000));
     walletPort.saveWallet(wallet3);
 
-    //when: 1->100->2, 2->300000->2, 3->100->1
-    final int threadCount = 100;
     final ExecutorService executorService1 = Executors.newFixedThreadPool(16);
     final ExecutorService executorService2 = Executors.newFixedThreadPool(16);
     final ExecutorService executorService3 = Executors.newFixedThreadPool(16);
-    final CountDownLatch latch = new CountDownLatch(300);
+    final CountDownLatch latch = new CountDownLatch(threadCount);
 
-    for (int i = 0; i < threadCount; i++) {
+    for (int i = 0; i < threadCount/3; i++) {
       executorService1.submit(() -> {
         try {
+          //1->2
           moneyService.sendMoney(
                   new SendMoneyServiceDto(
                           dto1.accountId(),
@@ -179,6 +193,7 @@ class MoneyServiceWithLockTest {
       });
       executorService2.submit(() -> {
         try {
+          //2->2
           moneyService.chargeMoney(dto2);
         } finally {
           latch.countDown();
@@ -186,6 +201,7 @@ class MoneyServiceWithLockTest {
       });
       executorService3.submit(() -> {
         try {
+          //3->1
           moneyService.sendMoney(
                   new SendMoneyServiceDto(
                           dto3.accountId(),
@@ -201,7 +217,6 @@ class MoneyServiceWithLockTest {
 
     latch.await();
 
-    //then: 1(3000), 2(303100), 3(2900)
     final Wallet savedWallet1 = walletPort.findWalletByAccountId(dto1.accountId());
     System.out.println("savedWallet1.getBalance() = " + savedWallet1.getBalance());
 
@@ -211,8 +226,8 @@ class MoneyServiceWithLockTest {
     final Wallet savedWallet3 = walletPort.findWalletByAccountId(dto3.accountId());
     System.out.println("savedWallet3.getBalance() = " + savedWallet3.getBalance());
 
-    assertThat(savedWallet1.getBalance()).isEqualTo(BigInteger.valueOf(3000));
-    assertThat(savedWallet2.getBalance()).isEqualTo(BigInteger.valueOf(303100));
-    assertThat(savedWallet3.getBalance()).isEqualTo(BigInteger.valueOf(2900));
+    assertThat(savedWallet1.getBalance()).isEqualTo(BigInteger.valueOf(balance1));
+    assertThat(savedWallet2.getBalance()).isEqualTo(BigInteger.valueOf(balance2));
+    assertThat(savedWallet3.getBalance()).isEqualTo(BigInteger.valueOf(balance3));
   }
 }
