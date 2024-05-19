19dd4edfdfdd01fb5c8abea946c8f3f248065795
LEEEUNJI <eunjilee3008@gmail.com>
2024-05-19 19:06:47 +0900

    [#20] fix: 부하테스트 시 데이터 정합성 불일치 문제 해결 - 1. EntityManager를 주입받아 영속성 컨텍스트를 초기화 한다.

--- money-service/src/main/java/com/hello/money/v1/service/MoneyDistributedLockService.java
+++ money-service/src/main/java/com/hello/money/v1/service/MoneyDistributedLockService.java
@@ -4,6 +4,7 @@ import com.hello.money.config.redis.DistributedLock;
 import com.hello.money.config.redis.DistributedMultiLock;
 import com.hello.money.domain.Transaction;
 import com.hello.money.domain.Wallet;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,12 @@ public class MoneyDistributedLockService {
 
   private final WalletPort walletPort;
   private final MoneyTransactionService transactionService;
+  private final EntityManager entityManager;
 
   @DistributedLock(key = "#walletId")
   public Wallet chargeMoneyWithLock(final Long walletId, final Transaction transaction) {
+
+    entityManager.clear();
 
     final Wallet wallet = walletPort.findWalletById(walletId);
     final Transaction successfulTransaction = transactionService.executeCharge(wallet, transaction);
@@ -26,6 +30,8 @@ public class MoneyDistributedLockService {
 
   @DistributedMultiLock(keys = {"#senderWalletId", "#receiverWalletId"})
   public Wallet sendMoneyWithLock(final Long senderWalletId, final Long receiverWalletId, final Transaction senderTransaction, final Transaction receiverTransaction) {
+
+    entityManager.clear();
 
     final Wallet senderWallet = walletPort.findWalletById(senderWalletId);
     final Wallet receiverWallet = walletPort.findWalletById(receiverWalletId);
